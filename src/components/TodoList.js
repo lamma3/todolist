a854dcdd0da637ec138ce70633dc2fe85b5265a1
Marco Lam <lamma3@oocl.com>
2020-04-22 16:02:48 +0800

    init components

---
+++ src/components/TodoList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class TodoList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default TodoList;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class TodoListItem extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default TodoListItem;
